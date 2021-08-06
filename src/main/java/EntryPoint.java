e261da082f4e33afbc1a38c8f7721ecaca012b2e
lakienvinh <vinh.lakien@stdio.vn>
2021-08-06 08:56:37 +0700

    + Solve exercise 4. + Solve exercise 5. + Solve exercise 6. + Solve exercise 7. + Solve exercise 8. + Solve exercise 9. + Solve exercise 10.

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,5 +1,92 @@
+import java.util.Scanner;
+
 public class EntryPoint {
  public static void main(String[] args) {
-     System.out.println("Hello");
+     Scanner scanner = new Scanner(System.in);
+
+
+//     Integer birthYear = scanner.nextInt();
+//     Integer currentYear = 2021;
+//     Integer age = currentYear - birthYear;
+//     System.out.println(age);
+
+
+//    int a = 5;
+//    int b = 6;
+//
+//    int sum = a + b;
+//    int sub = a - b;
+//    int mul = a * b;
+//    int div = a / b;
+//
+//    System.out.println("The result is: ");
+//    System.out.println(sum);
+//    System.out.println(sub);
+//    System.out.println(mul);
+//    System.out.println(div);
+
+
+//     int amount = 5;
+//     int price = 120000;
+//     final float vatRate = 10 / 100.0f;
+//
+//     int total = amount * price;
+//     int vat = (int) (total * vatRate);
+//
+//     System.out.println(total);
+//     System.out.println(vat);
+
+
+//    int math = 8;
+//    int mathFactor = 2;
+//
+//    int physic = 7;
+//    int physicFactor = 1;
+//
+//    int chemical = 9;
+//    int chemicalFactor = 1;
+//
+//    int finalScore = (math * mathFactor + physic * physicFactor + chemical * chemicalFactor) / (mathFactor + physicFactor + chemicalFactor);
+
+
+//    int r = 5;
+//    final double PI = 3.14159;
+
+//    double C = 2 * r * Math.PI;
+//    double S = Math.PI * r * r;
+
+
+//    int licensePlate = 73582;
+//
+//    int a0 = licensePlate / 10000;
+//    int a1 = (licensePlate / 1000) % 10;
+//    int a2 = (licensePlate / 100) % 10;
+//    int a3 = (licensePlate / 10) % 10;
+//    int a4 = licensePlate % 10;
+//    int sum = a0 + a1 + a2 + a3 + a4;
+//
+//    int point = sum % 10;
+
+
+    int a = 5;
+    int b = 6;
+
+     int min = a;
+     int max = b;
+
+     if (a > b) {
+         min = b;
+         max = a;
+     }
+
+     //    int min = Math.min(a, b);
+    //    int max = Math.max(a, b);
+
+
+//     int x = 21 / 5; // => 4
+//    int y = 21 % 5; // => 1
+
+//    21 / 4 => 4 * [5] + 1
+
  }
 }
